1b125f50a0c98f57f51ba5973f2ae3724b9e9892
Giuliano <giulianoandresbasso@gmail.com>
2024-05-27 15:41:58 -0300

    Agregado Ac Laurico y graficos. Working OK

--- analizador_TG.py
+++ analizador_TG.py
@@ -1,4 +1,4 @@
-
+#%%
 """
 Para archivos de Termogravimetria (TG)
 Giuliano Basso
@@ -12,7 +12,6 @@ from tkinter import filedialog
 import pandas as pd
 
 #%%
-
 def lector_TG(fpath):
     '''
     Lee archivo txt de la la medida de TG
@@ -48,6 +47,9 @@ def lector_TG(fpath):
     return meta,t,T,TGA
 #%% Levanto archivo TG de NPM en acido laurico
 meta0,t0,T0,TGA0 = lector_TG(os.path.join(os.getcwd(),'C5140836.TXT'))
+
+meta1,t1,T1,TGA1 = lector_TG(os.path.join(os.getcwd(),'C5231001_Laurico.TXT'))
+
 #busco indices del minimo
 indx_min=np.nonzero(TGA0==min(TGA0))
 Temp_min=T0[np.nonzero(TGA0==min(TGA0))]
@@ -63,10 +65,11 @@ if len(Temp_min)>1 :
 indices_xmin=np.nonzero(T0<=Temp_min-30)[0][-1]
 indices_xmax=np.nonzero(T0>=Temp_min+30)[0][0]
 TG_porcentual=100*TGA0/meta0['peso_mg']
-
+TG_porcentual_laurico=100*TGA1/meta1['peso_mg']
 #%grafico
 fig,ax =plt.subplots(constrained_layout=True)
 ax.plot(T0,TG_porcentual,'.-',label=meta0['nombre_de_muestra'])
+ax.plot(T1,TG_porcentual_laurico,'.-',label=meta1['nombre_de_muestra'])
 
 ax.set_ylabel('Peso (%)')
 ax.grid()
@@ -74,36 +77,37 @@ ax.set_xlabel('Temperatura (°C)')
 ax.legend()
 
 
-axins = ax.inset_axes([0.4, 0.4, 0.58, 0.5])
-axins.plot(T0,TG_porcentual,'.-',label=meta0['nombre_de_muestra'])
-# axins.axhline(0,0,1,c='k',lw=0.8)
-# axins.plot(H,m_norm,'.-',label='m norm')
-# axins.plot(H_aux,lineal(H_aux,chi,n),'r-',label=f'$\chi$ = {chi:.3e}\nHc = {H_aux[indx_M]:.3e} A/m')
-# axins.set_xlabel('H (G)')
-axins.grid()
-axins.legend(loc='lower right')
-# axins.legend()
-axins.set_ylim(TG_porcentual[indices_xmin],TG_porcentual[indices_xmax])
-axins.set_xlim(T0[indices_xmin],T0[indices_xmax])
-ax.indicate_inset_zoom(axins, edgecolor="black")
+# axins = ax.inset_axes([0.4, 0.4, 0.58, 0.5])
+# axins.plot(T0,TG_porcentual,'.-',label=meta0['nombre_de_muestra'])
+# # axins.axhline(0,0,1,c='k',lw=0.8)
+# # axins.plot(H,m_norm,'.-',label='m norm')
+# # axins.plot(H_aux,lineal(H_aux,chi,n),'r-',label=f'$\chi$ = {chi:.3e}\nHc = {H_aux[indx_M]:.3e} A/m')
+# # axins.set_xlabel('H (G)')
+# axins.grid()
+# axins.legend(loc='lower right')
+# # axins.legend()
+# axins.set_ylim(TG_porcentual[indices_xmin],TG_porcentual[indices_xmax])
+# axins.set_xlim(T0[indices_xmin],T0[indices_xmax])
+# ax.indicate_inset_zoom(axins, edgecolor="black")
 plt.title('TG - Ferrotec en Laurico 80')
 #%%
 rho=883 #densidad del acido laurico en mg/ml
-C0=np.mean(rho*TGA0[np.nonzero(TGA0==min(TGA0))]/TGA0[0])
+C0=np.mean(rho*(TGA0[np.nonzero(TGA0==min(TGA0))] - TGA1[np.nonzero(TGA0==min(TGA0))])/TGA0[0])
 print(meta0['nombre_de_muestra'],':',f'{C0:.2f}', 'g/L')
 #%%
 
 fig,ax =plt.subplots(constrained_layout=True)
 ax.plot(T0,TGA0,'.-',label=meta0['nombre_de_muestra'])
-
+ax.plot(T1,TGA1,'.-',label=meta1['nombre_de_muestra'])
+ax.axvline(Temp_min,0,1,color='tab:red',label=f'{Temp_min} °C')
 #ax.axhline(1.71,0,1,color='k',ls='-.',zorder=-1)
 ax.set_ylabel('Peso (%)')
 ax.grid()
 ax.set_xlabel('Temperatura (°C)')
 ax.legend()
 plt.title('TGA Ferroresinas')
-plt.xlim(790,805)
-plt.ylim(0,5)
+plt.xlim(Temp_min-30,Temp_min+30)
+plt.ylim(0,1.5)
 #plt.savefig('TGA_FR.png',dpi=200,facecolor='w')
 plt.show()
 # %%
